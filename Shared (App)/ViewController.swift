ea316f6203ffa5538162dc45420e0ee4443ff4d2
Ivan Apostolovski <ivan.apostolovski@gmail.com>
2025-08-31 23:50:55 +0200

    fix: fix project name in xcode pop up

--- Shared (App)/ViewController.swift
+++ Shared (App)/ViewController.swift
@@ -16,7 +16,7 @@ import SafariServices
 typealias PlatformViewController = NSViewController
 #endif
 
-let extensionBundleIdentifier = "ivanxho.actual-image-copy.Extension"
+let extensionBundleIdentifier = "ivanxho.image-copy.Extension"
 
 class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMessageHandler {
 
